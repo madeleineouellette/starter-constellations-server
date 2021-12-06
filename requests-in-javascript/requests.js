3cc3bf9c446fc10f95fbfe8354f413b4bbe64ed9
Madeleine Ouellette <madeleineouellette7@gmail.com>
2021-12-06 16:26:50 -0500

    updating requests.js

--- requests-in-javascript/requests.js
+++ requests-in-javascript/requests.js
@@ -11,4 +11,40 @@ axios
   .catch((error) => {
     console.log(error.message);
   });
-  
+
+/*const url = "http://localhost:9000/constellations";
+axios
+  .get(url)
+  .then((response) => {
+    const result = response.data.filter((constellation) => {
+      return constellation.starsWithPlanets < 10;
+    });
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });*/
+ /* const url = "http://localhost:9000/constellations";
+ /* axios
+    .post(url, {
+      name: "Ara",
+      meaning: "Altar",
+      starsWithPlanets: 7,
+      quadrant: "SQ3",
+    })
+    .then((response) => {
+      console.log(response.data);
+    });*/
+
+ //   const idToDelete = "33sJSj-";
+ //   axios.delete(`${url}${idToDelete}`);
+
+ 
+ 
+//const idToDelete = "s2dJ2op";
+// axios.delete(`${url}/${idToDelete}`);
+// axios.get(`${url}/${idToDelete}`);
+
+//axios.delete("http://localhost:9000/constellations/33sJSj-")
+
+    
