10c924be22d4010552e438d81fd890c39833a372
Madeleine Ouellette <madeleineouellette7@gmail.com>
2021-12-06 16:28:43 -0500

    qualified assessment

---
+++ Making_Requests_Requests_in_JavaScript_1/src/main.js
@@ -0,0 +1,11 @@
+const axios = require("../utils/axios");
+const BASE_URL = "http://localhost:5000";
+
+function getAllNames() {}
+
+function getConstellationsByQuadrant(quadrant) {}
+
+module.exports = {
+  getAllNames,
+  getConstellationsByQuadrant,
+};
