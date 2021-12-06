6e6cc1c5a7d88a1e37073a965e238a8228d1cab9
Madeleine Ouellette <madeleineouellette7@gmail.com>
2021-12-06 16:40:42 -0500

    commit 2

--- Making_Requests_Requests_in_JavaScript_1/src/main.js
+++ Making_Requests_Requests_in_JavaScript_1/src/main.js
@@ -1,12 +1,17 @@
 const axios = require("../utils/axios");
-const BASE_URL = "http://localhost:9000";
+const BASE_URL = "http://localhost:5000";
 
 function getAllNames() {
   axios
   .get(BASE_URL)
   .then((response) => {
-    console.log(response.name)
+    console.log(BASE_URL);
+    const result = []
+    for (let i = 0; i < response.length; i++){
+      result += response.name
+    }
   })
+  console.log(result)
 }
 
 function getConstellationsByQuadrant(quadrant) {}
