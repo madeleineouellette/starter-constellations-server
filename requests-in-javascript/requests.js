07ad7f3e97f212103ded273344ebadcfc37db1de
Madeleine Ouellette <madeleineouellette7@gmail.com>
2021-12-06 15:59:42 -0500

    commit

---
+++ requests-in-javascript/requests.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+
+const url = "http://localhost:5000/constellations";
+axios
+  .get(url)
+  .then((response) => {
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+  
